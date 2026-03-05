046fea4e0eaae21c1f84b9dc9e7390c4553eb942
skostadinov0141 <simeon.kostadinov0141@gmail.com>
2026-03-05 20:18:37 +0100

    feat: set dark mode using useColorMode
    Changes:
    - Import useColorMode from @vueuse/core
    - Set the color mode to 'dark' on app initialization
    
    Generated by GitHub Copilot

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
+import { useColorMode } from '@vueuse/core'
 
 const app = createApp(App)
 
@@ -11,5 +12,8 @@ app.use(createPinia())
 app.use(router)
 
 await router.isReady()
+
+const mode = useColorMode()
+mode.value = 'dark'
 
 app.mount('#app')
