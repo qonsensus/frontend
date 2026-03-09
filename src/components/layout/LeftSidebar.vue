bb6da1373815bdb1a90b2b5fc9f53b8bd9f72cd0
skostadinov0141 <simeon.kostadinov0141@gmail.com>
2026-03-09 21:10:32 +0100

    feat: add button group components and integration
    Changes:
    - Introduce ButtonGroup, ButtonGroupSeparator, and ButtonGroupText components.
    - Integrate ButtonGroup in LeftSidebar for microphone and headphones controls.
    
    Generated by GitHub Copilot

--- src/components/layout/LeftSidebar.vue
+++ src/components/layout/LeftSidebar.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="h-full w-20 border-r flex flex-col items-center">
+  <div class="h-full w-18 border-r flex flex-col items-center">
     <div class="grow" />
     <div class="flex flex-col items-center border-t gap-4 py-4 w-full">
-      <Avatar class="border flex items-center justify-center w-12 h-12">
+      <Avatar class="border flex items-center justify-center w-11 h-11">
         <AvatarImage src="@/assets/logo.png" />
         <AvatarFallback class="flex items-center justify-center">JD</AvatarFallback>
       </Avatar>
+      <ButtonGroup orientation="vertical">
+        <Button variant="outline" size="icon-lg" @click="isMicOn = !isMicOn">
+          <Mic v-if="isMicOn" />
+          <MicOff class="text-red-400" v-else />
+        </Button>
+        <Button variant="outline" size="icon-lg" @click="isHeadphonesOn = !isHeadphonesOn">
+          <Headphones v-if="isHeadphonesOn" />
+          <HeadphoneOff class="text-red-400" v-else />
+        </Button>
+      </ButtonGroup>
       <Button variant="outline" size="icon-lg">
         <Settings />
       </Button>
@@ -14,10 +24,14 @@
 </template>
 
 <script setup lang="ts">
-import { Settings } from 'lucide-vue-next'
-
+import { Settings, Mic, MicOff, Headphones, HeadphoneOff } from 'lucide-vue-next'
 import { Button } from '@/components/ui/button'
 import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import { ref } from 'vue'
+import { ButtonGroup } from '@/components/ui/button-group'
+
+const isMicOn = ref(true)
+const isHeadphonesOn = ref(true)
 </script>
 
 <style scoped></style>
